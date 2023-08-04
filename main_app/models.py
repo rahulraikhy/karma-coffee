933a5879e2b1eca89097720b19b84eb6ce074995
SimpsonRoss <thisisrosssimpson@gmail.com>
2023-08-04 09:07:25 -0400

    Add skeleton of project and set up templates, nav, pages and basic styling

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
-# Create your models here.
+ROAST = (
+    ('L', 'Light'),
+    ('M', 'Medium'),
+    ('D', 'Dark'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    price = models.IntegerField()
+    origin = models.CharField(max_length=100)
+    flavour = models.CharField(max_length=100)
+    roast = models.CharField(
+        max_length=1,
+        choices=ROAST,
+        default=ROAST[0][0]
+    )
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'product_id': self.id})
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def learn(request):
+    return render(request, 'learn.html')
+
+def products_index(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {
+        'products': products
+    })
+    
+
