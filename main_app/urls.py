99d90ce1efb9708254f2c3d4f1c6afe4a5184265
SimpsonRoss <thisisrosssimpson@gmail.com>
2023-08-04 09:49:38 -0400

    Add README.md

---
+++ main_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('learn/', views.learn, name='learn'),
+    path('products/', views.products_index, name='index'),
+]
