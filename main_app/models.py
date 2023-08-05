e659e245d94eba6c19e2d0eea476c897703f8957
SimpsonRoss <thisisrosssimpson@gmail.com>
2023-08-05 09:45:05 -0400

    Make Order and and OrderItem models

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,14 @@ ROAST = (
     ('D', 'Dark'),
 )
 
+ORDER_STATUS = (
+    ('C', 'Cart'),           # Order not yet completed
+    ('P', 'Pending'),        # Order completed, payment pending
+    ('S', 'Shipped'),        # Order shipped
+    ('D', 'Delivered'),      # Order delivered
+)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
@@ -27,4 +35,29 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'product_id': self.id})
-    
+
+
+# Ross - I'll use the customers order as the cart, until it's paid for, then update the status to pending..
+class Order(models.Model):
+    date = models.DateField('order date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=1, choices=ORDER_STATUS, default=ORDER_STATUS[0][0])
+
+    def __str__(self):
+        return f'Order #{self.id} for {self.user.username} - {self.get_status_display()}'
+
+    def get_absolute_url(self):
+        return reverse('order_detail', kwargs={'order_id': self.id})
+
+    def get_total(self):
+        return sum(item.product.price * item.quantity for item in self.orderitem_set.all())
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
