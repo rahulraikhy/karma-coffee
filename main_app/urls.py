3b2502680a923c34f3d5c670aafeed847aa82e7b
chajiiiii <j.cha1708@gmail.com>
2023-08-07 17:30:19 +0100

    Review functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('learn/', views.learn, name='learn'),
     path('products/', views.products_index, name='index'),
     path('products/<int:product_id>/', views.products_detail, name='detail'),
+    path('products/<int:product_id>/add_review', views.add_review, name='add_review'),
     path('accounts/signup/', views.signup, name='signup'),
     path('cart/', views.cart, name='cart'),
     path('products/<int:product_id>/add/',
